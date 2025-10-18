ff629508c8e945c800e1257e9410a7665e920c7b
Debora Carvalho <dbndy3@gmail.com>
2025-10-18 19:32:04 -0300

    MenuandGameOVER

---
+++ Scripts/game_over.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func _on_sair_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+
+func _on_tentar_dnv_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
